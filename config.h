741cafea9b8559de3e8e427d411d2dfb7e6d25b0
tjbo <tom@prototypable.ca>
2022-08-27 11:03:26 +0000

    add patch file

--- config.h
+++ config.h
@@ -57,10 +57,11 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "[M]",      monocle },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
 };
+
 
 /* key definitions */
 #define MODKEY Mod1Mask
